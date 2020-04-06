94baa4e8afcf8ec97c5c861339554fed2f71f5be
Dilawar Singh <dilawars@ncbs.res.in>
2020-04-06 18:47:05 +0530

    Only python3 on OSX.

--- build_wheels_osx.sh
+++ build_wheels_osx.sh
@@ -10,7 +10,6 @@ export PATH=/usr/local/bin:$PATH
 brew update || echo "Failed to update brew"
 brew install gsl  || brew upgrade gsl 
 brew install python@3 || echo "Failed to install python3"
-brew install python@2 || echo "Failed to install python2"
 
 # Following are to remove numpy; It is breaking the build on Xcode9.4.
 # brew uninstall gdal postgis || echo "Failed to uninstall gdal/postgis"
@@ -34,33 +33,31 @@ rm -rf $WHEELHOUSE && mkdir -p $WHEELHOUSE
 DELOCATE_WHEEL=/usr/local/bin/delocate-wheel
 
 # Always prefer brew version.
-for _py in 3 2; do
-    PYTHON=/usr/local/bin/python$_py
+PYTHON=/usr/local/bin/python3
 
-    if [ ! -f $PYTHON ]; then
-        echo "Not found $PYTHON"
-        continue
-    fi
+if [ ! -f $PYTHON ]; then
+    echo "Not found $PYTHON"
+    continue
+fi
 
 
-    $PYTHON -m pip install setuptools --upgrade --user
-    $PYTHON -m pip install wheel --upgrade --user
-    $PYTHON -m pip install numpy --upgrade --user
-    $PYTHON -m pip install twine  --upgrade  --user
+$PYTHON -m pip install setuptools --upgrade --user
+$PYTHON -m pip install wheel --upgrade --user
+$PYTHON -m pip install numpy --upgrade --user
+$PYTHON -m pip install twine  --upgrade  --user
 
-    PLATFORM=$($PYTHON -c "import distutils.util; print(distutils.util.get_platform())")
-    ( 
-        cd $MOOSE_SOURCE_DIR
-        $PYTHON setup.py build_ext 
-        export GSL_USE_STATIC_LIBRARIES=1
-        $PYTHON setup.py bdist_wheel --skip-build 
-        $DELOCATE_WHEEL -v dist/*.whl -w $WHEELHOUSE
-        rm -rf dist/*.whl
-    )
+PLATFORM=$($PYTHON -c "import distutils.util; print(distutils.util.get_platform())")
+( 
+    cd $MOOSE_SOURCE_DIR
+    $PYTHON setup.py build_ext 
+    export GSL_USE_STATIC_LIBRARIES=1
+    $PYTHON setup.py bdist_wheel --skip-build 
+    $DELOCATE_WHEEL -v dist/*.whl -w $WHEELHOUSE
+    rm -rf dist/*.whl
+)
 
-    if [ ! -z "$PYMOOSE_PYPI_PASSWORD" ]; then
-        echo "Did you test the wheels? I am uploading anyway ..."
-        $PYTHON -m twine upload -u bhallalab -p $PYMOOSE_PYPI_PASSWORD \
-            $WHEELHOUSE/pymoose*.whl || echo "Failed to upload to PyPi"
-    fi
-done
+if [ ! -z "$PYMOOSE_PYPI_PASSWORD" ]; then
+    echo "Did you test the wheels? I am uploading anyway ..."
+    $PYTHON -m twine upload -u bhallalab -p $PYMOOSE_PYPI_PASSWORD \
+        $WHEELHOUSE/pymoose*.whl || echo "Failed to upload to PyPi"
+fi
